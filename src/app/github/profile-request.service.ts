9fa63a41b356684995e7dbd39a9870a27f723caa
Michael Korir <mikor22@gmail.com>
2019-05-06 17:17:49 +0300

    create a promise for values

--- src/app/github/profile-request.service.ts
+++ src/app/github/profile-request.service.ts
@@ -37,3 +37,25 @@ constructor(private http:HttpClient) {
     bio:string;
 
 }
+
+    let promise = new Promise((resolve,reject)=>{
+      this.http.get<UserInfo>(environment.apiUrl+this.username +environment.access_token).toPromise().then(response=>{
+        this.user.login= response.login
+        this.user.avatar_url = response.avatar_url
+        this.user.public_repos = response.public_repos
+        this.user.followers=response.followers
+        this.user.following=response.following
+        this.user.html_url=response.html_url
+        this.user.created_at=response.created_at
+        this.user.bio=response.bio
+
+        resolve()
+       },error=>{
+         this.user.login = "Error Fetching Data"
+         this.user.avatar_url = "Error Fetching Data"
+         this.user.public_repos = "Error"
+         reject(error)
+       })
+    })
+     return promise
+   }
